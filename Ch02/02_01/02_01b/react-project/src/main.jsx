8efb7aae637cc2397fff5c70deacc6d7b4bc4908
Thounda Craig <thoundac94@gmail.com>
2025-09-11 06:17:19 -0400

    basic example of the useState hook

--- Ch02/02_01/02_01b/react-project/src/main.jsx
+++ Ch02/02_01/02_01b/react-project/src/main.jsx
@@ -2,7 +2,4 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-const [firstAnimal, , third] = ["giraffe", "zebra", "bear"];
-console.log(third);
-
 createRoot(document.getElementById("root")).render(<App />);
