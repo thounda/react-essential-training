5b2193bc941806a2a334c6649957fafcac262f60
Thounda Craig <thoundac94@gmail.com>
2025-09-12 16:23:43 -0400

    passing data as props

--- Ch05/next-react-project/src/app/hotels/page.js
+++ Ch05/next-react-project/src/app/hotels/page.js
@@ -1,8 +1,32 @@
-export default function Page() {
+async function getData() {
+  const res = fetch("https://snowtooth-hotel-api.fly.dev");
+  return (await res).json();
+}
+
+function HotelBlock({ name, capacity }) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>{capacity}</p>
+    </div>
+  );
+}
+
+export default async function Page() {
+  const data = await getData();
   return (
     <main>
       <div>
         <h1>Hotel Details</h1>
+        <div>
+          {data.map((hotel) => (
+            <HotelBlock
+              key={hotel.id}
+              name={hotel.name}
+              capacity={hotel.capacity}
+            />
+          ))}
+        </div>
       </div>
     </main>
   );
