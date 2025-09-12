27de1d5822a94a9c4ca805267d51b67d17c87b71
Thounda Craig <thoundac94@gmail.com>
2025-09-12 15:59:35 -0400

    fetching data in React applications

--- Ch05/next-react-project/src/app/mountain/page.js
+++ Ch05/next-react-project/src/app/mountain/page.js
@@ -1,7 +1,29 @@
-export default function Page() {
+async function getData() {
+  const res = await fetch("https://snowtooth-api-rest.fly.dev");
+  return res.json();
+}
+
+export default async function Page() {
+  const data = await getData();
   return (
     <main>
       <h1>Lift Status Info</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>List Name</th>
+            <th>Current Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((lift) => (
+            <tr key={lift.id}>
+              <td>lift.name</td>
+              <td>lift.status</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </main>
   );
 }
